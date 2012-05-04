bbab45e3aae25d0d25b5576f7c1ca2e9c94794ef
Ricky Elrod <ricky@elrod.me>
2012-05-03 23:54:52 -0400

    Add some more tests.

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -6,7 +6,26 @@ describe Cabrillo do
     log = Cabrillo.parse_file(valid_file)
 
     log.should be_an_instance_of(Cabrillo)
-    log.to_hash.should be_an_instance_of(Hash)
-    log.to_hash[:version].should == "3.0"
+    hashified_log = log.to_hash
+
+    hashified_log.should be_an_instance_of(Hash)
+
+    hashified_log[:version].should == "3.0"
+    log.version.should == "3.0"
+
+    hashified_log[:callsign].should == "W8UPD"
+    log.callsign.should == "W8UPD"
+
+    hashified_log[:address].should be_an_instance_of(Array)
+    log.address.should be_an_instance_of(Array)
+
+    hashified_log[:address].first.should == "501 Zook Hall"
+    log.address.first.should == "501 Zook Hall"
+
+    hashified_log[:soapbox].should be_an_instance_of(Array)
+    log.soapbox.should be_an_instance_of(Array)
+
+    log.soapbox.size.should == 2
+    hashified_log[:soapbox].size.should == 2
   end
 end
